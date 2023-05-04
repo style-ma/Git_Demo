6a56f0c7ef57d4a2d84eeb0eb8b7d41b00af3156
style_ma <style_ma@163.com>
2023-05-04 10:57:26 +0800

    Initial commit

---
+++ Git_Demo01/src/main/java/Git_Demo_Test.java
@@ -0,0 +1,13 @@
+/**
+ * @title: Git_Demo_Test
+ * @Author msm
+ * @Date: 2023/5/4 10:51
+ * @Version 2018.3.6
+ */
+public class Git_Demo_Test {
+    public static void main(String[] args) {
+        System.out.println("hello world !");
+    }
+}
+
+
